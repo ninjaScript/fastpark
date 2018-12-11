b3d4b9416a2f9a6eb01598f091dad4dc58dc1fbe
radwan-1 <radwanbara@gmail.com>
2018-12-11 14:53:38 +0200

    (feat) updating feature and ongoing to the next adittional feature

--- react-ui/src/components/CustomerService.js
+++ react-ui/src/components/CustomerService.js
@@ -144,10 +144,10 @@ export default class CustomerService extends React.Component {
   render() {
     const { email, name, phoneNumber, comments } = this.state;
     const enabled =
-          name.length > 0 &&
-          email.length > 0 &&
-          phoneNumber.length > 0 && 
-          comments.length > 0;
+          name.length == 0 &&
+          email.length == 0 &&
+          phoneNumber.length == 0 && 
+          comments.length == 0;
     return (
 
       <div className="customerService">
@@ -155,7 +155,7 @@ export default class CustomerService extends React.Component {
             <img src={Customer}/>
           </div>
           <div className="customer">
-                <Form>    
+                <Form onSubmit={!enabled}>    
                     <FormGroup>
                         <Col sm={10}>
                             <Col className="headCustomer">
@@ -185,7 +185,7 @@ export default class CustomerService extends React.Component {
                         </Col>
                     </FormGroup>
                     <FormGroup  handleOnClick={this.handleOnClick} >
-                            <Button type = "submit" id="submit"  disabled={!enabled} onClick={this.handleOnClick.bind(this)}>{this.state.toggleButtonSpin && <i className="fa fa-spinner fa-spin"></i>}SEND</Button>
+                            <Button type = "submit" id="submit"  onClick={this.handleOnClick.bind(this)}>{this.state.toggleButtonSpin && <i className="fa fa-spinner fa-spin"></i>}SEND</Button>
                                 {this.state.toggleButton && <p align="center" style={{marginLeft: "-100px"}}  className="label"><i class="fa fa-check icon"></i><b>Thanks, we'll be in touch as soon as possible.</b></p>}
                     </FormGroup>
                 </Form>
